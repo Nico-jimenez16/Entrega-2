4528754d8bca2cb0c163adee4d55a803b3be3627
Nico-jimenez16 <jimeneznicolas520@gmail.com>
2022-05-06 13:15:00 -0300

    comit 2 tabla

--- app.js
+++ app.js
@@ -25,39 +25,24 @@ Vue.component('my-header', {
 
 Vue.component('products', {
     template: /* html */ `    
-        <div>
-            <h1 class="flex w-full justify-center items-center text-3xl p-4">{{ tabla1.title }}</h1>
-            <h1 class="flex w-full justify-center items-center text-3xl p-4">{{ tabla2.title }}</h1>
+        <div class="mb-8">
+            <h1 class="flex w-full justify-center items-center text-3xl p-4">{{ tabla.title }}</h1>
             <div>
                 <table class="w-full table-auto">
                     <thead class="bg-black text-white">
                         <tr>
-                            <th v-for="(head, i) of tabla1.headtable" :key="i" scope="col">{{head}}</th>
-                        </tr>
-                        <tr>
-                            <th v-for="(head, i) of tabla2.headtable" :key="i" scope="col">{{head}}</th>
+                            <th v-for="(head, i) of tabla.headtable" :key="i" scope="col">{{head}}</th>
                         </tr>
                     </thead>
 
                     <!-- Tabla de Productos -->
 
                     <tbody class="bg-yellow-300 text-2xl">
-                        <tr v-for="(producto, index) of tabla1.productos" :key="index">
+                        <tr v-for="(producto, index) of tabla.productos" :key="index">
                             <td>{{index}}</td>
                             <td><img class="w-1/5 h-32" :src="producto.url"></td>
                             <td>{{producto.descripcion}}</td>
-                            <td>{{producto.precio}}</td>
-                        </tr>
-                    </tbody>
-
-                    <!-- Tabla de Usuario -->
-
-                    <tbody class="bg-yellow-300 text-2xl">
-                        <tr v-for="(usuario, index) of tabla2.usuarios" :key="index">
-                            <td>{{index}}</td>
-                            <td>{{usuario.nombre}}</td>
-                            <td>{{usuario.apellido}}</td>
-                            <td>{{usuario.edad}}</td>
+                            <td>$ {{producto.precio}}</td>
                         </tr>
                     </tbody>
                     
@@ -67,11 +52,7 @@ Vue.component('products', {
     `,
     
     props: {
-        tabla1:{
-            type: Object,
-            required: true
-        },
-        tabla2:{
+        tabla:{
             type: Object,
             required: true
         }
@@ -85,48 +66,52 @@ const app = new Vue ({
         return {
             titulo: 'Hola , soy Nicolas Jimenez',
             entrega: 'Esta es la Segunda entrega - Componentes con VueJs',
-            tabla1: {
-                title: 'Tabla de datos con Componentes - Hamburguesas',
-                headtable: ['Index', 'Imagen', 'Descripcion', 'Precio'],
-                productos: 
-                [
-                    {
-                        url: './image/hamburguesa.jpg',
-                        descripcion: 'Hamburguesa simple',
-                        precio: 100
-                    },
-                    {
-                        url: './image/hamburguesa.jpg',
-                        descripcion: 'Hamburguesa doble',
-                        precio: 200
-                    },
-                    {
-                        url: './image/hamburguesa.jpg',
-                        descripcion: 'Hamburguesa triple',
-                        precio: 300
-                    }
-                ]
+            one: {
+                tabla: {
+                    title: 'Tabla de datos con Componentes - Hamburguesas',
+                    headtable: ['Index', 'Imagen', 'Descripcion', 'Precio'],
+                    productos: 
+                    [
+                        {
+                            url: './image/hamburguesa.jpg',
+                            descripcion: 'Hamburguesa simple',
+                            precio: 100
+                        },
+                        {
+                            url: './image/hamburguesa.jpg',
+                            descripcion: 'Hamburguesa doble',
+                            precio: 200
+                        },
+                        {
+                            url: './image/hamburguesa.jpg',
+                            descripcion: 'Hamburguesa triple',
+                            precio: 300
+                        }
+                    ]
+                }
             },
-            tabla2: {
-                title: 'Tabla de datos con Componentes - Usuarios',
-                headtable: ['Index', 'Nombre', 'Apellido', 'Edad'],
-                usuarios: [
-                    {
-                        nombre: 'Felipe',
-                        apellido: 'Jimenez',
-                        edad: 58
-                    },
-                    {
-                        nombre: 'Andres',
-                        apellido: 'Ramirez',
-                        edad: 35
-                    },
-                    {
-                        nombre: 'Juan',
-                        apellido: 'Rodriguez',
-                        edad: 17
-                    }
-                ]
+            two: {
+                tabla: {
+                    title: 'Tabla de datos con Componentes - Usuarios',
+                    headtable: ['Index', 'Nombre', 'Apellido', 'Edad'],
+                    productos: [
+                        {
+                            url: './image/audi.jpg',
+                            descripcion: 'Audi A4',
+                            precio: 58000
+                        },
+                        {
+                            url: './image/audi.jpg',
+                            descripcion: 'Audi A5',
+                            precio: 68000
+                        },
+                        {
+                            url: './image/audi.jpg',
+                            descripcion: 'Audi A5',
+                            precio: 78000
+                        }
+                    ]
+                }
             }
         }
     }
